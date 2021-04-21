de9bae65e22a129147a137b4f2ef2b5af522d3b0
sanoti <gievoy01@mail.ru>
2021-04-22 02:47:57 +0400

    :tropical_fish:

---
+++ Task 3/task_3.1/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace task_3
+{
+    //class MainClass //WEAKEST LINK
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        List<int> Krug = new List<int>();
+
+    //        Console.Write("N = ");
+    //        if (!int.TryParse(Console.ReadLine(), out int N) || N < 0)
+    //        {
+    //            Console.WriteLine("Incorrect input");
+    //            return;
+    //        }
+
+    //        //Console.Write("Enter the number of people to be removed in each round: ");
+    //        //if (!int.TryParse(Console.ReadLine(), out int Jumping) || Jumping < 2)
+    //        //{
+    //        //    Console.WriteLine("Incorrect input");
+    //        //    return;
+    //        //}
+
+    //        for (int i = 0; i < N; i++)
+    //        {
+    //            Krug.Add(i + 1);
+    //        }
+
+    //        Console.WriteLine("Circle of people is generated. We start deleting every second");
+
+    //        for (int i = 0, j = 0; N > 1; i++, N--)
+    //        {
+    //            Console.WriteLine($"Turn {++j}. Person is deleted. People left: {N - 1}");
+
+    //            if (i >= N)
+    //            {
+    //                i = 0;
+    //            }
+    //            Krug.RemoveAt((i + 1) % N);
+    //            //if (Jumping > N)
+    //            //    break;
+    //        }
+
+    //        Console.WriteLine("Game over. It is impossible to deleting more people");
+    //    }
+    //}
+
+    class MainClass //TEXT ANALYSIS
+    {
+        public static void Main(string[] args)
+        {
+            string Input;
+            Console.WriteLine("Enter the sentece:");
+            Input = Console.ReadLine();
+
+            string[] StrMas = Input.ToLower().Split(new[] { ' ', '!', '?', ':', ',', '.', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] Count = new int[StrMas.Length];
+
+            for (int i = 0; i < StrMas.Length; i++)
+            {
+                while (StrMas[i] == "!" && i < StrMas.Length - 1) { //пропуск повторяющихся слво
+                    Count[i] = -1;
+                    i++;
+                }
+                for (int j = i + 1; j < StrMas.Length; j++)
+                {
+                    if (StrMas[i] == StrMas[j])
+                    {
+                        Count[i]++;
+                        StrMas[j] = "!"; //"удаление" слов, которые уже были
+                    }
+                }
+            }
+
+            //for (int i = 0; i < Count.Length; i++) //вывод счетчиков
+            //{
+            //    Console.Write(Count[i] + " ");
+            //}
+
+            int Max1 = 0, Max2 = 1;
+            for (int i = 1; i < Count.Length; i++) //максимальный счетчик
+            {
+                if (Count[i] > Count[Max1])
+                {
+                    Max1 = i;
+                }
+                   
+            }
+            for (int i = 2; i < Count.Length; i++) //предмакчимальный счетчик
+            {
+                if (Count[i] > Count[Max2] && Count[i] != Count[Max1])
+                {
+                    Max2 = i;
+                }
+                    
+            }
+            Console.WriteLine();
+            Console.WriteLine(Max1 + " " + Max2);
+
+            Console.WriteLine("The most popular words:");
+            Console.WriteLine($"{StrMas[Max1]} - {Count[Max1] + 1} times");
+            Console.WriteLine($"{StrMas[Max2]} - {Count[Max2] + 1} times");
+
+
+            //List<int> K = new List<int>() { 1, 4, 5, 1, 2, 1, 9 };
+
+            //K.Remove(1);
+
+            //foreach(int i in K)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //K.Remove(1);
+            //Console.WriteLine();
+            //foreach (int i in K)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+        }
+    }
+
+
+
+
+
+}
---
+++ Task 3/task_3.2/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace task_3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DynamicArray<int> array = new DynamicArray<int>(3);
+            List<int> list = new List<int>() { 10, 14 };
+
+            //Console.WriteLine(array.Length());
+            //Console.WriteLine(array.Capacity());
+            //Console.WriteLine();
+
+            array.Add(5);
+            array.Add(3);
+            array.Add(15);
+            //array.AddRange(list);
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+            array.Insert(21, 1);
+
+            Console.WriteLine();
+            Console.WriteLine(array.Length());
+            Console.WriteLine(array.Capacity());
+
+
+            //array.Remove(3);
+
+            Console.WriteLine();
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < array.Length(); i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+
+        }
+    }
+
+    class DynamicArray<T> : IEnumerable, IEnumerable<T>//, IEnumerator
+    {
+        T[] array;
+        private int size = 0;
+
+        public object Current => throw new NotImplementedException();
+
+        public DynamicArray() // 1
+        {
+            array = new T[8];
+        }
+
+        public DynamicArray(int Length) // 2
+        {
+            array = new T[Length];
+        }
+
+        public DynamicArray(ICollection<T> Collection) //3
+        {
+            array = new T[Collection.Count];
+            Collection.CopyTo(array, 0);
+
+        }
+
+        private void Increase()
+        {
+            T[] NewArray = new T[2 * Capacity()];
+            for (int i = 0; i < size; i++)
+            {
+                NewArray[i] = array[i];
+            }
+
+            array = NewArray;
+
+            //throw new ArgumentOutOfRangeException();
+        }
+
+        private void Increase(int neskolko)
+        {
+            T[] NewArray = new T[neskolko * Capacity()];
+            for (int i = 0; i < size; i++)
+            {
+                NewArray[i] = array[i];
+            }
+
+            array = NewArray;
+
+            //throw new ArgumentOutOfRangeException();
+        }
+
+        public void Add(T element) //4
+        {
+            if (size >= Capacity())
+            {
+                Increase();
+                //Console.WriteLine("Array has been increased");
+            }
+            array[size] = element;
+            size++;
+        }
+
+        public void AddRange(ICollection<T> Collection) //5
+        {
+            if (Collection.Count > (Capacity() - Length()))
+            {
+                Increase(Collection.Count / Capacity() + 2); //отношение длины добавляеммой коллекции к обьему массива 
+                                                             // +2, тк интовое деление и необходимость корректного расширения
+                                                             //Console.WriteLine("Array has been increased for collection");
+            }
+
+            //Collection.CopyTo(array, size); 
+            foreach (var el in Collection)
+            {
+                array[size] = el;
+                size++;
+            }
+        }
+
+        public bool Remove(T element) //6
+        {
+            int pos = -1;
+            for (int i = 0; i < size; i++)
+            {
+                if (element.Equals(array[i]))
+                {
+                    pos = i;
+                    break;
+                }
+            }
+
+            if (pos == -1)
+                return false;
+
+            for (int i = pos; i < size - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            size--;
+            array[size] = default;
+            return true;
+        }
+
+        public bool Insert(T element, int pos) //7
+        {
+            if ((pos + 1) > Capacity())
+                throw new ArgumentOutOfRangeException();
+
+            if ((pos + 1) == Capacity() || Length() == Capacity())
+            {
+                Increase();
+            }
+
+            for (int i = size; i >= pos; i--)
+            {
+                array[i] = array[i - 1];
+            }
+            array[pos] = element;
+            size++;
+
+            return true;
+        }
+
+        public int Length() //8
+        {
+            return size;
+        }
+
+        public int Capacity() //9
+        {
+            return array.Length;
+        }
+
+        public IEnumerator GetEnumerator() //10
+        {
+            for (int i = 0; i < Length(); i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (IEnumerator<T>)GetEnumerator();
+        }
+
+        public T this[int index] //11
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+
+    }
+}
+
+
+
